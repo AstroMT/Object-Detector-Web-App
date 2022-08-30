7d1926de8db1b51fc18c83e2f0f7e87e6aee11bd
AstroMT <81778387+AstroMT@users.noreply.github.com>
2022-08-30 12:35:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function directToBedroom() {
+    window.location = "bedroom.html";
+}
+
+function directToTV() {
+    window.location = "tv.html";
+}
+
+function directToDesk() {
+    window.location = "desk.html";
+}
+
+function directToPatio() {
+    window.location = "patio.html";
+}
+
+function directToDiningRoom() {
+    window.location = "dining_room.html";
+}
